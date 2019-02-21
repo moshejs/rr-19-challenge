f8ca1c45511bbe88f5cabca5defe4fe944ada90e
Moshe <moshemalka@mail.adelphi.edu>
2019-02-20 21:42:19 -0500

    update module to include new components

--- project-dashboard/src/app/app.module.ts
+++ project-dashboard/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemRecordService } from 'src/services/in-memory-records.service';
+import { RecordRowComponent } from './record-row/record-row.component';
+import { RecordsComponent } from './records/records.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordRowComponent,
+    RecordsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemRecordService, { delay: 500 }),
   ],
