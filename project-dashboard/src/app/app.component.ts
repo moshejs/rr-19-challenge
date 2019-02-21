be4d7d0a1d33520823ad4eafd82910f3f30363bd
Moshe <moshemalka@mail.adelphi.edu>
2019-02-20 21:42:42 -0500

    update app component to include record table

--- project-dashboard/src/app/app.component.ts
+++ project-dashboard/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecordsService } from 'src/services/records.service';
+import { Observable } from 'rxjs';
+
+import { Record } from '../models/record.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'project-dashboard';
+export class AppComponent implements OnInit {
+  constructor(private recordsService: RecordsService) {
+  }
+  title = 'Project Dashboard';
+  records$: Observable<Record[]>;
+
+  ngOnInit() {
+    this.records$ = this.getRecords();
+  }
+
+  getRecords(): Observable<Record[]> {
+    return this.recordsService.getRecords();
+  }
 }
