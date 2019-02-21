da7efee9110203908bd6e45d40793742351a0ed7
Moshe <moshemalka@mail.adelphi.edu>
2019-02-20 21:37:57 -0500

    update in-memory logic

--- project-dashboard/src/services/in-memory-records.service.ts
+++ project-dashboard/src/services/in-memory-records.service.ts
@@ -1,10 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import * as recordsData from '../data/records.json';
+import { mockRecords } from '../data/records';
 import { Record } from '../models/record.model';
 
 export class InMemRecordService implements InMemoryDbService {
   createDb() {
-    const records: Record[] = recordsData;
+    const records: Record[] = <Record[]>mockRecords;
 
     return {records};
   }
