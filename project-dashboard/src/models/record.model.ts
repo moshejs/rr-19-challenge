47e39973a691d64b15f5b6be170151206f49174e
Moshe <moshemalka@mail.adelphi.edu>
2019-02-20 21:37:48 -0500

    update records model

--- project-dashboard/src/models/record.model.ts
+++ project-dashboard/src/models/record.model.ts
@@ -1,6 +1,7 @@
 export class Record {
     title: string;
     division: string;
+    project_owner: string;
     budget: number;
     status: 'archived' | 'new' | 'working' | 'delivered';
     created: string;
