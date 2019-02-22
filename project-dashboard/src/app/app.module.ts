84b26b15226ea1793782653ddf5f4955fbf6be07
Moshe <moshemalka@mail.adelphi.edu>
2019-02-22 10:38:08 -0500

    changes

--- project-dashboard/src/app/app.module.ts
+++ project-dashboard/src/app/app.module.ts
@@ -12,15 +12,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { EditDropdownCellComponent } from './edit-dropdown-cell/edit-dropdown-cell.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
 
+import { EditDropdownCellComponent } from './edit-dropdown-cell/edit-dropdown-cell.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecordRowComponent,
     RecordsComponent,
-    EditDropdownCellComponent
+    EditDropdownCellComponent,
+    StatisticsComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,13 @@ import { EditDropdownCellComponent } from './edit-dropdown-cell/edit-dropdown-ce
     HttpClientInMemoryWebApiModule.forRoot(InMemRecordService, { delay: 500 }),
     MatSelectModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    FlexLayoutModule,
+    NgxChartsModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
